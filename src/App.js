10edb09017519b6831d8aae581b425ad69f9025e
unknown <alex131986@gmail.com>
2020-06-22 22:23:10 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends React.Component {
+  state = {
+    advice: "",
+  };
+
+  componentDidMount() {
+    this.fatchApiForAdvice();
+  }
+
+  fatchApiForAdvice = () => {
+    axios
+      .get("	https://api.adviceslip.com/advice")
+      .then((res) => {
+        const { advice } = res.data.slip;
+        console.log(advice);
+        //if the value of state and the const is the same we can put just the name of the const (instet of ({advice: advice}))
+        this.setState({ advice });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  render() {
+    const { advice } = this.state;
+    return (
+      <div className="app">
+        <div className="card">
+          <h1 className="heading">{advice}</h1>
+          <button className="button" onClick={this.fatchApiForAdvice}>
+            <span>GIVE ADVICE</span>
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
